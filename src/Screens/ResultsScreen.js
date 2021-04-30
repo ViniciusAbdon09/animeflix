28d97fc7bb95313db83c320e030ac4c014e538b5
Vinicius Abdon <vnabdon@gmail.com>
2021-04-30 16:08:05 -0300

    Results screen created

--- src/Screens/ResultsScreen.js
+++ src/Screens/ResultsScreen.js
@@ -1,12 +1,80 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  Text,
+  View,
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+import CardAnime from '../components/CardAnime';
 
-const ResultsScreen = () => {
-    return (
-        <View>
-            <Text>Results</Text>
-        </View>
-    )
-}
+const api = 'https://kitsu.io/api/edge/';
+
+const ResultsScreen = (props) => {
+  const {search} = props.route.params;
+  const [animes, setAnimes] = useState([]);
+
+  useEffect(() => {
+    fetch(`${api}anime?filter[text]=${search}&page[limit]=9`)
+      .then(res => res.json({}))
+      .then(res => setAnimes(res));
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.safe}>
+      <Text style={styles.text}>Search by {search}...</Text>
+      <View style={styles.display}>
+        <FlatList
+          data={animes.data}
+          keyExtractor={item => item.id}
+          numColumns={3}
+          renderItem={({item}) => {
+            return (
+              <TouchableOpacity
+                style={styles.item}
+                onPress={() => {
+                  props.navigation.navigate('Details', {anime: item});
+                }}>
+                <View>
+                  <CardAnime
+                    key={item.id}
+                    title={item.attributes.canonicalTitle}
+                    urlImage={item.attributes.posterImage.small}
+                  />
+                </View>
+              </TouchableOpacity>
+            );
+          }}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  item: {
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+    flexGrow: 1,
+    overflow: 'hidden',
+    flexBasis: 0,
+    padding: 4,
+    margin: 4,
+  },
+  display: {
+    margin: 0,
+  },
+  safe: {
+    flex: 1,
+    backgroundColor: 'black',
+    opacity: 0.84,
+  },
+  text: {
+    color: 'white',
+    textAlign: 'center',
+    marginTop: 5,
+  },
+});
 
 export default ResultsScreen;
