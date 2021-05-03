5d3c2bcb5d31726070d657a9f1537a9c38bdd83a
Vinicius Abdon <vnabdon@gmail.com>
2021-05-03 12:08:27 -0300

    Moved touchableOpacity into the component cardAnime

--- src/components/CardAnime.js
+++ src/components/CardAnime.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native';
 
 export default props => {
+  const {navigate} = props
   return (
-    <>
+    <TouchableOpacity
+      style={styles.item}
+      onPress={() => {
+        props.navigate('Details', {animeData: props.anime});
+      }}>
       <View>
         <Image
           source={{uri: props.urlImage}}
@@ -15,12 +20,21 @@ export default props => {
       <View style={{marginLeft: 8, marginRight: 8}}>
         <Text style={styles.text}>{props.title}</Text>
       </View>
-    </>
+    </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   text: {
     color: '#fff',
+  },
+  item: {
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+    flexGrow: 1,
+    overflow: 'hidden',
+    flexBasis: 0,
+    padding: 4,
+    margin: 4,
   },
 });
